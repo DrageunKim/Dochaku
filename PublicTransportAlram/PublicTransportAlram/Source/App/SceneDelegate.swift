800d3ddbd3974a1237f0e2bf4364aa0d98393ad1
DragonYG <kioswe92@gmail.com>
2023-02-23 20:36:19 +0900

    feat: MainViewController - 기본 UI 구현

--- PublicTransportAlram/PublicTransportAlram/Source/App/SceneDelegate.swift
+++ PublicTransportAlram/PublicTransportAlram/Source/App/SceneDelegate.swift
@@ -14,9 +14,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let window = UIWindow(windowScene: windowScene)
         
-        window.rootViewController = ViewController()
+        let window = UIWindow(windowScene: windowScene)
+        let rootView = MainViewController()
+        let navigationController = UINavigationController.init(rootViewController: rootView)
+        
+        window.rootViewController = navigationController
         window.makeKeyAndVisible()
         
         self.window = window
--- PublicTransportAlram/PublicTransportAlram/Source/Scene/MainViewController.swift
+++ PublicTransportAlram/PublicTransportAlram/Source/Scene/MainViewController.swift
@@ -8,11 +8,135 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    private let nowStationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "현재역 :"
+        label.font = .preferredFont(forTextStyle: .headline)
+        label.numberOfLines = 1
+        label.textAlignment = .right
+        label.textColor = .label
+        return label
+    }()
+    private let targetStationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "도착역 :"
+        label.font = .preferredFont(forTextStyle: .headline)
+        label.numberOfLines = 1
+        label.textAlignment = .right
+        label.textColor = .label
+        return label
+    }()
+    private let nowStationBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.backgroundColor = .systemBackground
+        return searchBar
+    }()
+    private let targetStationBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.backgroundColor = .systemBackground
+        return searchBar
+    }()
+    private let okButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .label
+        button.setTitle("설정", for: .normal)
+        button.layer.backgroundColor = UIColor.systemMint.cgColor
+        button.layer.borderWidth = 0.5
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    private let initButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .label
+        button.setTitle("초기화", for: .normal)
+        button.layer.backgroundColor = UIColor.systemMint.cgColor
+        button.layer.borderWidth = 0.5
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    private let nowStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .fill
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.spacing = 5
+        return stackView
+    }()
+    private let targetStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .fill
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.spacing = 5
+        return stackView
+    }()
+    private let buttonStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .center
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 20
+        return stackView
+    }()
+    private let settingStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 10
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemBackground
+        configureView()
+        configureStackView()
+        configureLayout()
     }
 }
 
+// MARK: - View & Layout Configure
+
+extension MainViewController {
+    private func configureView() {
+        view.backgroundColor = .systemBackground
+        
+        navigationItem.title = "🔔   도착 알림   🔔"
+    }
+    
+    private func configureStackView() {
+        nowStackView.addArrangedSubview(nowStationLabel)
+        nowStackView.addArrangedSubview(nowStationBar)
+        
+        targetStackView.addArrangedSubview(targetStationLabel)
+        targetStackView.addArrangedSubview(targetStationBar)
+        
+        buttonStackView.addArrangedSubview(okButton)
+        buttonStackView.addArrangedSubview(initButton)
+        
+        settingStackView.addArrangedSubview(nowStackView)
+        settingStackView.addArrangedSubview(targetStackView)
+        settingStackView.addArrangedSubview(buttonStackView)
+    }
+    
+    private func configureLayout() {
+        view.addSubview(settingStackView)
+        
+        NSLayoutConstraint.activate([
+            nowStackView.widthAnchor.constraint(equalTo: settingStackView.widthAnchor),
+            targetStackView.widthAnchor.constraint(equalTo: settingStackView.widthAnchor),
+            buttonStackView.widthAnchor.constraint(equalTo: settingStackView.widthAnchor),
+            
+            settingStackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.75),
+            settingStackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.23),
+            settingStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            settingStackView.topAnchor.constraint(
+                equalTo: view.topAnchor,
+                constant: view.bounds.height * 0.18
+            )
+        ])
+    }
+}
