897d241c21bcb8c81f91d7dc1decfbcbeed6e0ce
DragonYG <kioswe92@gmail.com>
2023-02-23 19:55:21 +0900

    init

---
+++ PublicTransportAlram/PublicTransportAlram/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PublicTransportAlram
+//
+//  Created by yonggeun Kim on 2023/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
+
