49e75a1f2f67a561ff156a034ebebbcf9789dd5c
DragonYG <kioswe92@gmail.com>
2023-03-20 01:39:33 +0900

    refactor: MapView-titleLabel, 요일&시간설정 Label&Button 추가

--- PublicTransportAlram/PublicTransportAlram/Source/Presentation/TabBar/Map/MapViewController.swift
+++ PublicTransportAlram/PublicTransportAlram/Source/Presentation/TabBar/Map/MapViewController.swift
@@ -17,8 +17,15 @@ class MapViewController: UIViewController {
         stackView.alignment = .center
         stackView.axis = .vertical
         stackView.distribution = .fill
-        stackView.spacing = 10
+        stackView.spacing = 20
         return stackView
+    }()
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "⏰ 알림 설정"
+        label.textColor = .label
+        label.font = .preferredFont(forTextStyle: .headline).withSize(20)
+        return label
     }()
     private let segmentedControl: UISegmentedControl = {
         let control = UISegmentedControl(items: ["지하철", "버스", "주소"])
@@ -53,6 +60,79 @@ class MapViewController: UIViewController {
         location.requestWhenInUseAuthorization()
         return location
     }()
+    private let dateTimeStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.alignment = .fill
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 5
+        return stackView
+    }()
+    private let dateStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .center
+        stackView.axis = .horizontal
+        stackView.distribution = .equalCentering
+        stackView.spacing = 20
+        return stackView
+    }()
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .systemBackground
+        label.text = "요일 설정"
+        label.textColor = .label
+        label.font = .preferredFont(forTextStyle: .headline)
+        return label
+    }()
+    private let dataSettingLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .systemBackground
+        label.text = "[월, 화, 수, 목]"
+        label.textColor = .label
+        label.font = .preferredFont(forTextStyle: .subheadline)
+        return label
+    }()
+    private let dateSettingButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemBackground
+        button.setImage(.add, for: .normal)
+        button.setTitleColor(.label, for: .normal)
+        button.titleLabel?.font = .preferredFont(forTextStyle: .headline)
+        return button
+    }()
+    private let timeStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .center
+        stackView.axis = .horizontal
+        stackView.distribution = .equalCentering
+        stackView.spacing = 20
+        return stackView
+    }()
+    private let timeLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .systemBackground
+        label.text = "시간 설정"
+        label.textColor = .label
+        label.font = .preferredFont(forTextStyle: .headline)
+        return label
+    }()
+    private let timeSettingLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .systemBackground
+        label.text = "00:00 ~ 24:00"
+        label.textColor = .label
+        label.font = .preferredFont(forTextStyle: .subheadline)
+        return label
+    }()
+    private let timeSettingButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemBackground
+        button.setImage(.add, for: .normal)
+        button.setTitleColor(.label, for: .normal)
+        button.titleLabel?.font = .preferredFont(forTextStyle: .headline)
+        return button
+    }()
     private let buttonStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -61,6 +141,12 @@ class MapViewController: UIViewController {
         stackView.distribution = .fillEqually
         stackView.spacing = 20
         return stackView
+    }()
+    private let borderLine: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .label
+        return view
     }()
     private let okButton: UIButton = {
         let button = UIButton()
@@ -178,11 +264,23 @@ extension MapViewController {
     private func configureView() {
         view.backgroundColor = .systemBackground
     }
-    
+
     private func configureStackView() {
+        topStackView.addArrangedSubview(titleLabel)
         topStackView.addArrangedSubview(segmentedControl)
         topStackView.addArrangedSubview(locationSearchBar)
         topStackView.addArrangedSubview(mapView)
+        
+        dateStackView.addArrangedSubview(dateLabel)
+        dateStackView.addArrangedSubview(dataSettingLabel)
+        dateStackView.addArrangedSubview(dateSettingButton)
+        
+        timeStackView.addArrangedSubview(timeLabel)
+        timeStackView.addArrangedSubview(timeSettingLabel)
+        timeStackView.addArrangedSubview(timeSettingButton)
+        
+        dateTimeStackView.addArrangedSubview(dateStackView)
+        dateTimeStackView.addArrangedSubview(timeStackView)
         
         buttonStackView.addArrangedSubview(okButton)
         buttonStackView.addArrangedSubview(initButton)
@@ -190,25 +288,37 @@ extension MapViewController {
     
     private func configureLayout() {
         view.addSubview(topStackView)
+        view.addSubview(dateTimeStackView)
+        view.addSubview(borderLine)
         view.addSubview(buttonStackView)
         
         NSLayoutConstraint.activate([
             topStackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
-            topStackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.7),
+            topStackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.6),
             topStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             topStackView.topAnchor.constraint(
                 equalTo: view.topAnchor,
-                constant: view.bounds.height * 0.12
+                constant: view.bounds.height * 0.1
             ),
             
             segmentedControl.widthAnchor.constraint(equalTo: topStackView.widthAnchor, multiplier: 0.8),
             locationSearchBar.widthAnchor.constraint(equalTo: topStackView.widthAnchor),
             mapView.widthAnchor.constraint(equalTo: topStackView.widthAnchor),
             
+            dateTimeStackView.widthAnchor.constraint(equalTo: topStackView.widthAnchor, multiplier: 0.9),
+            dateTimeStackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1),
+            dateTimeStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            dateTimeStackView.topAnchor.constraint(equalTo: topStackView.bottomAnchor, constant: 10),
+            
+            borderLine.widthAnchor.constraint(equalTo: topStackView.widthAnchor),
+            borderLine.heightAnchor.constraint(equalToConstant: 1),
+            borderLine.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            borderLine.topAnchor.constraint(equalTo: dateTimeStackView.bottomAnchor, constant: 10),
+            
             buttonStackView.widthAnchor.constraint(equalTo: topStackView.widthAnchor, multiplier: 0.9),
             buttonStackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05),
             buttonStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            buttonStackView.topAnchor.constraint(equalTo: topStackView.bottomAnchor, constant: 10)
+            buttonStackView.topAnchor.constraint(equalTo: borderLine.bottomAnchor, constant: 10)
         ])
     }
 }
